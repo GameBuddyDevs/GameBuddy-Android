706f76ca659cf8c96258c8491b4fbe52b62fa827
Burak Karaduman <karadumanburak000@gmail.com>
2023-03-08 18:05:21 +0300

    - Module added for retrofit - Extensions added for retrofit

--- app/src/main/java/com/example/gamebuddy/util/Extensions.kt
+++ app/src/main/java/com/example/gamebuddy/util/Extensions.kt
@@ -1,7 +1,37 @@
 package com.example.gamebuddy.util
 
+import retrofit2.HttpException
+
+
+// Retrofit Extensions
+fun <T> handleUseCaseException(exception: Throwable): DataState<T> {
+    exception.printStackTrace()
+    when (exception) {
+        is HttpException -> {
+            return DataState.error(
+                response = Response(
+                    message = exception.message(),
+                    uiComponentType = UIComponentType.Dialog(),
+                    messageType = MessageType.Error()
+                )
+            )
+        }
+        else -> {
+            return DataState.error(
+                response = Response(
+                    message = exception.message ?: "Unknown error",
+                    uiComponentType = UIComponentType.Dialog(),
+                    messageType = MessageType.Error()
+                )
+            )
+        }
+    }
+}
+
+
+// Queue Extensions
 fun StateMessage.isMessageExistInQueue(
-    queue: Queue<StateMessage>
+    queue: Queue<StateMessage>,
 ): Boolean {
 
     queue.items.forEach { stateMessage ->
