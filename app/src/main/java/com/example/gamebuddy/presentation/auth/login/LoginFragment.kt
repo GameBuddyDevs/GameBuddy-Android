392aff83a72fc1ff83e7df80c0288c8b40c55aec
KaanKarakass <kaan.karakass1@outlook.com>
2023-03-19 22:37:28 +0300

    Created and written LoginEvents class and LoginFragment class edited.

--- app/src/main/java/com/example/gamebuddy/presentation/auth/login/LoginFragment.kt
+++ app/src/main/java/com/example/gamebuddy/presentation/auth/login/LoginFragment.kt
@@ -4,18 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.gamebuddy.R
+import androidx.fragment.app.viewModels
+import com.example.gamebuddy.databinding.FragmentLoginBinding
+import com.example.gamebuddy.presentation.auth.BaseAuthFragment
+import com.example.gamebuddy.presentation.auth.register.RegisterViewModel
 
 
-class LoginFragment : Fragment() {
+class LoginFragment : BaseAuthFragment() {
+
+    //private val viewModel : LoginViewModel by viewModels()
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater,container,false)
+
+        binding.btnLogin.setOnClickListener {
+            login()
+        }
+        return binding.root
+    }
+    private fun login(){
+
     }
 
 
