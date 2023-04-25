91b40d95e560333a5a4ce3bdd0b12872c635ac1f
burak <karadumanburak000@gmail.com>
2023-04-25 19:18:31 +0300

    - Bug fix on login errors

--- app/src/main/java/com/example/gamebuddy/session/SessionManager.kt
+++ app/src/main/java/com/example/gamebuddy/session/SessionManager.kt
@@ -34,7 +34,7 @@ class SessionManager @Inject constructor(
     init {
         sessionScope.launch {
             appDataStore.getValue(Constants.LAST_AUTH_USER)?.let { email ->
-                Timber.d("startup-logic: Found previous auth user: $email")
+                Timber.d("ALOOO managerke: Found previous auth user: $email")
                 onTriggerEvent(SessionEvents.CheckPreviousAuthUser(email = email))
             } ?: onUserNotFound()
         }
@@ -55,14 +55,19 @@ class SessionManager @Inject constructor(
             checkPreviousAuthUser.execute(email = email).onEach { dataState ->
                 _sessionState.value = state?.copy(isLoading = dataState.isLoading)
 
+                Timber.d("ALOOOO stateMessagesss $dataState")
+
                 dataState.data?.let { authToken ->
-                    Timber.d("startup-logic: Found previous auth user: $authToken")
+                    Timber.d("ALOOO S Found previous auth user: $authToken")
                     _sessionState.value = state?.copy(authToken = authToken)
                     onTriggerEvent(SessionEvents.ValidateToken(authToken = authToken))
                 }
 
                 dataState.stateMessage?.let { stateMessage ->
+                    Timber.d("ALOOOO No previous auth user found ${stateMessage.response.message}")
                     if (stateMessage.response.message == "Done checking for previously authenticated user.") {
+                        Timber.d("ALOOOO No previous auth user found inside if")
+                        _sessionState.value = state?.copy(actionType = AuthActionType.LOGIN)
                         onFinishedCheckingForPreviousAuthUser()
                     } else {
                         appendToMessageQueue(stateMessage)
