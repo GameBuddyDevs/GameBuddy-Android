9c07c0829cd9f6a6862a025e858baf373992d503
burak <karadumanburak000@gmail.com>
2023-05-22 21:18:33 +0300

    - item_community_detail.xml added

--- app/src/main/java/com/example/gamebuddy/presentation/main/communitydetail/CommunityDetailFragment.kt
+++ app/src/main/java/com/example/gamebuddy/presentation/main/communitydetail/CommunityDetailFragment.kt
@@ -5,10 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.gamebuddy.R
-
+import com.example.gamebuddy.databinding.FragmentCommunityBinding
+import com.example.gamebuddy.databinding.FragmentCommunityDetailBinding
+import com.example.gamebuddy.presentation.main.community.CommunityViewModel
+import com.example.gamebuddy.presentation.main.community.PostAdapter
 
 class CommunityDetailFragment : Fragment() {
+
+    //private val viewModel: CommunityDetailViewModel by viewModels()
+
+    private var _binding: FragmentCommunityDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private var postAdapter: PostAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +27,11 @@ class CommunityDetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_community_detail, container, false)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
